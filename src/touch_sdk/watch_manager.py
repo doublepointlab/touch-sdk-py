53d7fded4ff66f41f25f171de07557905d6eda0e
Felix Bade <flix.bade@gmail.com>
2022-12-01 12:57:55 +0200

    Add comment why INTERACTION_SERVICE is needed.

--- src/touch_sdk/watch_manager.py
+++ src/touch_sdk/watch_manager.py
@@ -10,6 +10,9 @@ from touch_sdk.protobuf.watch_output_pb2 import Update, Gesture, TouchEvent
 
 
 # GATT related UUIDs
+# INTERACTION_SERVICE is needed for scanning while the Wear OS
+# app is backwards compatible. Only one service UUID can be
+# advertised.
 INTERACTION_SERVICE = "008e74d0-7bb3-4ac5-8baf-e5e372cced76"
 PROTOBUF_SERVICE = "f9d60370-5325-4c64-b874-a68c7c555bad"
 PROTOBUF_OUTPUT = "f9d60371-5325-4c64-b874-a68c7c555bad"

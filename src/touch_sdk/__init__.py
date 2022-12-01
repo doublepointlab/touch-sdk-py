c40f49aacfcb5c5b239f47f7a492b417ece7f0de
Felix Bade <flix.bade@gmail.com>
2022-12-01 16:24:32 +0200

    Add docstring to __init__.py

--- src/touch_sdk/__init__.py
+++ src/touch_sdk/__init__.py
@@ -1,3 +1,12 @@
 from touch_sdk.watch import Watch
 
 __version__ = '0.0.17'
+
+__doc__ = """Connects to Port 6 Touch SDK compatible Bluetooth devices
+
+You can download a controller app for Wear OS from
+https://play.google.com/store/apps/details?id=io.port6.watchbridge
+
+This module will connect to the Wear OS app via Bluetooth LE.
+
+See also https://port6.io/sdk"""

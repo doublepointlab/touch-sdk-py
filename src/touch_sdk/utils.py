7dd76cb8ac8827aa72738e3cc72d5f049d5e019e
Felix Bade <flix.bade@gmail.com>
2023-02-23 16:58:29 +0200

    Fix itertools.pairwise for py3.8 and py3.9

---
+++ src/touch_sdk/utils.py
@@ -0,0 +1,8 @@
+from itertools import tee
+
+# For Python 3.8 and 3.9
+def pairwise(iterable):
+    # pairwise('ABCDEFG') --> AB BC CD DE EF FG
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)

bf92b61704cc459f83373863b1a2b377f696ae14
Felix Bade <flix.bade@gmail.com>
2023-03-10 15:25:00 +0200

    Reorder Watch methods for readability
    The ones calling each other are close by.

--- src/touch_sdk/watch.py
+++ src/touch_sdk/watch.py
@@ -87,6 +87,8 @@ class Watch:
         if update.HasField("info"):
             self.hand = Hand(update.info.hand)
 
+    # Custom characteristics
+
     async def _subscribe_to_custom_characteristics(self, client):
         if self.custom_data is None:
             return
@@ -106,6 +108,11 @@ class Watch:
 
         self.on_custom_data(characteristic.uuid, content)
 
+    def on_custom_data(self, uuid: str, content: Tuple):
+        """Receive data from custom characteristics"""
+
+    # Misc
+
     @staticmethod
     def _protovec2_to_tuple(vec):
         return (vec.x, vec.y)
@@ -121,12 +128,14 @@ class Watch:
     async def _on_protobuf(self, message):
         self._proto_on_sensors(message.sensorFrames, message.unixTime)
         self._proto_on_gestures(message.gestures)
-        self._proto_on_button_events(message.buttonEvents)
         self._proto_on_touch_events(message.touchEvents)
+        self._proto_on_button_events(message.buttonEvents)
         self._proto_on_rotary_events(message.rotaryEvents)
 
         if message.HasField("info"):
             self._proto_on_info(message.info)
+
+    # Sensor events
 
     def _proto_on_sensors(self, frames, timestamp):
         frame = frames[-1]
@@ -148,54 +157,10 @@ class Watch:
         self.on_sensors(sensor_frame)
         self._on_arm_direction_change(sensor_frame)
 
-    def _proto_on_gestures(self, gestures):
-        if any(g.type == Gesture.GestureType.TAP for g in gestures):
-            self.on_tap()
-
-    def _proto_on_button_events(self, buttons):
-        if any(b.id == 0 for b in buttons):
-            self.on_back_button()
-
-    def _proto_on_touch_events(self, touch_events):
-        for touch in touch_events:
-            coords = self._protovec2_to_tuple(touch.coords[0])
-            if touch.eventType == TouchEvent.TouchEventType.BEGIN:
-                self.on_touch_down(*coords)
-            elif touch.eventType == TouchEvent.TouchEventType.END:
-                self.on_touch_up(*coords)
-            elif touch.eventType == TouchEvent.TouchEventType.MOVE:
-                self.on_touch_move(*coords)
-            elif touch.eventType == TouchEvent.TouchEventType.CANCEL:
-                self.on_touch_cancel(*coords)
-
-    def _proto_on_rotary_events(self, rotary_events):
-        for rotary in rotary_events:
-            self.on_rotary(-rotary.step)
-
-    def _proto_on_info(self, info):
-        self.hand = Hand(info.hand)
-
-    def _write_input_characteristic(self, data, client):
-        loop = asyncio.get_running_loop()
-        loop.create_task(
-            self._async_write_input_characteristic(PROTOBUF_INPUT, data, client)
-        )
-
-    async def _async_write_input_characteristic(self, characteristic, data, client):
-        if client:
-            await client.write_gatt_char(characteristic, data)
-
-    @staticmethod
-    def _create_haptics_update(intensity, length):
-        clamped_intensity = min(max(intensity, 0.0), 1.0)
-        clamped_length = min(max(int(length), 0), 5000)
-        haptic_event = HapticEvent()
-        haptic_event.type = HapticEvent.HapticType.ONESHOT
-        haptic_event.length = clamped_length
-        haptic_event.intensity = clamped_intensity
-        input_update = InputUpdate()
-        input_update.hapticEvent.CopyFrom(haptic_event)
-        return input_update
+    def on_sensors(self, sensor_frame: SensorFrame):
+        """Callback when accelerometer, gyroscope, gravity, orientation, and
+        magnetic field are changed. Guaranteed to have values for everything but
+        magnetic field information in every update."""
 
     def _on_arm_direction_change(self, sensor_frame: SensorFrame):
         def normalize(vector):
@@ -214,27 +179,31 @@ class Watch:
 
         self.on_arm_direction_change(delta_x, delta_y)
 
-    def on_custom_data(self, uuid: str, content: Tuple):
-        """Receive data from custom characteristics"""
-
-    def trigger_haptics(self, intensity: float, duration_ms: int):
-        """Trigger vibration haptics on the watch.
-
-        intensity: between 0 and 1
-        duration_ms: between 0 and 5000"""
-        input_update = self._create_haptics_update(intensity, duration_ms)
-        self._write_input_characteristic(input_update.SerializeToString(), self.client)
-
-    def on_sensors(self, sensor_frame: SensorFrame):
-        """Callback when accelerometer, gyroscope, gravity, orientation, and
-        magnetic field are changed. Guaranteed to have values for everything but
-        magnetic field information in every update."""
-
     def on_arm_direction_change(self, delta_x: float, delta_y: float):
         """Gyroscope-based raycasting output. Called after sensor updates."""
 
+    # Gestures
+
+    def _proto_on_gestures(self, gestures):
+        if any(g.type == Gesture.GestureType.TAP for g in gestures):
+            self.on_tap()
+
     def on_tap(self):
         """Called when the tap gesture happens."""
+
+    # Touch screen
+
+    def _proto_on_touch_events(self, touch_events):
+        for touch in touch_events:
+            coords = self._protovec2_to_tuple(touch.coords[0])
+            if touch.eventType == TouchEvent.TouchEventType.BEGIN:
+                self.on_touch_down(*coords)
+            elif touch.eventType == TouchEvent.TouchEventType.END:
+                self.on_touch_up(*coords)
+            elif touch.eventType == TouchEvent.TouchEventType.MOVE:
+                self.on_touch_move(*coords)
+            elif touch.eventType == TouchEvent.TouchEventType.CANCEL:
+                self.on_touch_cancel(*coords)
 
     def on_touch_down(self, x: float, y: float):
         """Touch screen touch starts."""
@@ -248,12 +217,61 @@ class Watch:
     def on_touch_cancel(self, x: float, y: float):
         """Touch screen touch becomes a swipe gesture that goes to another view."""
 
-    def on_rotary(self, direction: int):
-        """Rotary dial around the watch screen is turned.
+    # Button
 
-        direction: +1 for clockwise, -1 for counterclockwise."""
+    def _proto_on_button_events(self, buttons):
+        if any(b.id == 0 for b in buttons):
+            self.on_back_button()
 
     def on_back_button(self):
         """Back button of the watch is pressed and released.
 
         Wear OS does not support separate button down and button up events."""
+
+    # Rotary
+
+    def _proto_on_rotary_events(self, rotary_events):
+        for rotary in rotary_events:
+            self.on_rotary(-rotary.step)
+
+    def on_rotary(self, direction: int):
+        """Rotary dial around the watch screen is turned.
+
+        direction: +1 for clockwise, -1 for counterclockwise."""
+
+    # Info
+
+    def _proto_on_info(self, info):
+        self.hand = Hand(info.hand)
+
+    # Haptics
+
+    def trigger_haptics(self, intensity: float, duration_ms: int):
+        """Trigger vibration haptics on the watch.
+
+        intensity: between 0 and 1
+        duration_ms: between 0 and 5000"""
+        input_update = self._create_haptics_update(intensity, duration_ms)
+        self._write_input_characteristic(input_update.SerializeToString(), self.client)
+
+    @staticmethod
+    def _create_haptics_update(intensity, length):
+        clamped_intensity = min(max(intensity, 0.0), 1.0)
+        clamped_length = min(max(int(length), 0), 5000)
+        haptic_event = HapticEvent()
+        haptic_event.type = HapticEvent.HapticType.ONESHOT
+        haptic_event.length = clamped_length
+        haptic_event.intensity = clamped_intensity
+        input_update = InputUpdate()
+        input_update.hapticEvent.CopyFrom(haptic_event)
+        return input_update
+
+    def _write_input_characteristic(self, data, client):
+        loop = asyncio.get_running_loop()
+        loop.create_task(
+            self._async_write_input_characteristic(PROTOBUF_INPUT, data, client)
+        )
+
+    async def _async_write_input_characteristic(self, characteristic, data, client):
+        if client:
+            await client.write_gatt_char(characteristic, data)

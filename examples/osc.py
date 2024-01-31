896417c04f999491fbbb93efb558d46dbf809828
Simon Frübis <simon.fruebis@me.com>
2024-01-31 16:25:57 +0100

    Added missing OSC features

--- examples/osc.py
+++ examples/osc.py
@@ -12,8 +12,38 @@ osc_client = SimpleUDPClient(ip, port)
 class MyWatch(Watch):
     def on_sensors(self, sensors):
         angular_velocity = sensors.angular_velocity
+        gravity = sensors.gravity
+        acceleration = sensors.acceleration
+        orientation = sensors.orientation
         osc_client.send_message("/angular-velocity", angular_velocity)
-        print(angular_velocity)
+        osc_client.send_message("/gravity", gravity)
+        osc_client.send_message("/acceleration", acceleration)
+        osc_client.send_message("/orientation", orientation)        
+
+    def on_tap(self):
+        osc_client.send_message("/tap", 1)
+        print('tap')
+
+    def on_touch_down(self, x, y):
+        osc_client.send_message("/touch-down", [x, y])
+        print('touch down', x, y)
+
+    def on_touch_up(self, x, y):
+        osc_client.send_message("/touch-up", [x, y])
+        print('touch up', x, y)
+
+    def on_touch_move(self, x, y):
+        osc_client.send_message("/touch-move", [x, y])
+        print('touch move', x, y)
+
+    def on_rotary(self, direction):
+        osc_client.send_message("/rotary", direction)
+        print('rotary', direction)
+
+    def on_back_button(self):
+        osc_client.send_message("/back-button", 1)
+        self.trigger_haptics(1.0, 20)
+        print('back button')
 
 watch = MyWatch()
 watch.start()

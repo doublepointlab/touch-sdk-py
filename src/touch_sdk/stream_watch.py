870e43ec3b5bc5016b2ec9d4e752ba1516e20671
VisaJE <eemil@live.com>
2024-03-01 10:29:57 +0200

    Added disable input argument

--- src/touch_sdk/stream_watch.py
+++ src/touch_sdk/stream_watch.py
@@ -1,6 +1,5 @@
 import base64
 from functools import partial
-import select
 import binascii
 import sys
 import asyncio
@@ -25,11 +24,12 @@ class StreamWatch:
     Watch also parses the data that comes over Bluetooth and returns it through
     callback methods."""
 
-    def __init__(self, name_filter=None):
+    def __init__(self, name_filter=None, disable_input=False):
         """Creates a new instance of StreamWatch. Does not start scanning for Bluetooth
         devices. Use Watch.start to enter the scanning and connection event loop.
 
-        Optional name_filter connects only to watches with that name (case insensitive)
+        Optional name_filter connects only to watches with that name (case insensitive).
+        If disable_input is true, watch listens to no inputs.
         """
         self._connector = WatchConnector(
             self._on_approved_connection, self._on_protobuf, name_filter
@@ -38,6 +38,7 @@ class StreamWatch:
         self._client = None
         self._stop_event = None
         self._event_loop = None
+        self._disable_input = disable_input
 
     def start(self):
         """Blocking event loop that starts the Bluetooth scanner
@@ -66,14 +67,17 @@ class StreamWatch:
         asyncio_atexit.register(self.stop)
 
         await self._connector.start()
-        task1 = asyncio.create_task(self._input_loop())  # Wrap coroutines in tasks
-        task2 = asyncio.create_task(self._wait_and_stop())
-        _, pending = await asyncio.wait(
-            [task2, task1],
-            return_when=asyncio.FIRST_COMPLETED,
-        )
-        for p in pending:
-            p.cancel()
+        if not self._disable_input:
+            task1 = asyncio.create_task(self._input_loop())  # Wrap coroutines in tasks
+            task2 = asyncio.create_task(self._wait_and_stop())
+            _, pending = await asyncio.wait(
+                [task2, task1],
+                return_when=asyncio.FIRST_COMPLETED,
+            )
+            for p in pending:
+                p.cancel()
+        else:
+            await self._wait_and_stop()
 
     async def _wait_and_stop(self):
         assert self._stop_event
@@ -147,11 +151,12 @@ def main():
     parser = ArgumentParser()
     parser.add_argument("--name-filter", type=str, default=None)
     parser.add_argument("--debug-level", type=int, default=logging.CRITICAL + 1)
+    parser.add_argument("--disable-input", action="store_true")
     args = parser.parse_args()
     logging.basicConfig(level=args.debug_level)
 
     try:
-        StreamWatch(args.name_filter).start()
+        StreamWatch(args.name_filter, args.disable_input).start()
     except KeyboardInterrupt:
         pass
 
